b11248c8e199688c535ffac8669375fa5ac3e8c9
vorayash <vorayash9028@gmail.com>
2022-02-12 23:40:16 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Shop from './components/Shop';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+   <>
+    {/* <Shop/> */}
+    <Navbar/>
+   </>
+  );
+}
+
+export default App;
